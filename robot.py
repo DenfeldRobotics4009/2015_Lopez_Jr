a34381c530983234a991c484aff9b0cb74dcaf01
Austin Fuller <auxchar@gmail.com>
2015-01-11 20:51:22 -0600

    Simpler this way.

--- robot.py
+++ robot.py
@@ -9,9 +9,9 @@ def precision_mode(controller_input, button_state):
     """copied from CubertPy, b/c it worked"""
     if controller_input <= dead_zone and controller_input >= -dead_zone:
         return 0
-    elif controller_input >= dead_zone:
+    elif controller_input > 0:
         controller_input = ((controller_input-dead_zone)/(1-dead_zone))**3
-    elif controller_input <= dead_zone:
+    elif controller_input < 0:
         controller_input = ((-controller_input-dead_zone)/(dead_zone-1))**3
 
     if button_state:
