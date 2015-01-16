b24cafd4beedfff8179f9c096f6b5338a4e555fa
Austin Fuller <auxchar@gmail.com>
2015-01-15 20:16:51 -0600

    Methods need 'self' as a parameter.

--- autonomous_utilities.py
+++ autonomous_utilities.py
@@ -4,39 +4,39 @@ class Auto(object):
     def __init__(self, robot):
         self.robot = robot
 
-    def forward(sec):
+    def forward(self, sec):
         """drives robot forward for sec seconds... needs to be tested"""
         self.robot.drive.mecanumDrive_Cartesian(0, .5, 0, 0)
         wpilib.Timer.delay(sec)
         self.robot.drive.mecanumDrive_Cartesian(0, 0, 0, 0)
 
-    def tote_grabba():
+    def tote_grabba(self):
         """Sets the window motor to half for .5 of a sec to grab the tote."""
         self.robot.window_motor.set(.5)
         wpilib.Timer.delay(.5)
         self.robot.window_motor.set(0)
 
-    def tote_releasa():
+    def tote_releasa(self):
         """Releases tote by doing the reverse of tote_grabba"""
         tote_lower(.05)
         self.robot.window_motor.set(-.5)
         wpilib.Timer.delay(.5)
         self.robot.window_motor.set(0)
 
-    def tote_lift(sec):
+    def tote_lift(self, sec):
         self.robot.aux_left.set(.5)
         self.robot.aux_right.set(.5)
         wpilib.Timer.delay(sec)
         self.robot.aux_left.set(0)
         self.robot.aux_right.set(0)
 
-    def tote_lower(sec):
+    def tote_lower(self, sec):
         self.robot.aux_left.set(-.5)
         self.robot.aux_right.set(-.5)
         wpilib.Timer.delay(sec)
         self.robot.aux_left.set(0)
         self.robot.aux_right.set(0)
 
-    def can_slappa():
+    def can_slappa(self):
         """Slappa da can, mon."""
         #do some funky driving here.
