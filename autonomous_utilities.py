98d89da1492ac7f02c5ae5549bda00889420a826
Austin Fuller <auxchar@gmail.com>
2015-01-17 10:03:37 -0600

    Removed obsolete code.

--- autonomous_utilities.py
+++ autonomous_utilities.py
@@ -26,29 +26,19 @@ class Auto(object):
         self.robot.grabba_pid.setSetpoint(grabba_out)
         self.robot.grabba_pid.enable()
         tote_lower(.05)
-        self.robot.window_motor.set(-.5)
         wpilib.Timer.delay(.5)
-        self.robot.window_motor.set(0)
         self.robot.grabba_pid.disable()
 
     def tote_lift(self, sec): #Stack 23 things on top of other things.
         self.robot.lift_pid.setSetpoint(lift_up)
         self.robot.lift_pid.enable()
-        self.robot.aux_left.set(.5)
-        self.robot.aux_right.set(.5)
         wpilib.Timer.delay(sec)
-        self.robot.aux_left.set(0)
-        self.robot.aux_right.set(0)
         self.robot.lift_pid.disable()
 
     def tote_lower(self, sec):
         self.robot.lift_pid.setSetpoint(lift_down)
         self.robot.lift_pid.enable()
-        self.robot.aux_left.set(-.5)
-        self.robot.aux_right.set(-.5)
         wpilib.Timer.delay(sec)
-        self.robot.aux_left.set(0)
-        self.robot.aux_right.set(0)
         self.robot.lift_pid.disable()
 
     def can_slappa(self):
