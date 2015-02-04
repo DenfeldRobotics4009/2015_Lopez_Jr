ae81f5adb880eb3f45cb80dcdea276b6628ffd76
nikolojedison <nikolojedison@gmail.com>
2015-02-04 23:41:12 +0000

    A wild Autonomous appeared. Wild Autonomous used TEST ME

--- commands/autonomous.py
+++ commands/autonomous.py
@@ -1,32 +1,42 @@
 __author__ = 'nikolojedison'
 #Temporary copypasta from robot.py, since I'm doing some housekeeping
 #over there. -Nik    
+#    three_tote = self.smart_dashboard.getBoolean("3 Tote Auto", defaultValue=False) 
+#    test_switch = self.smart_dashboard.getBoolean("Test Switch", defaultValue=False)
+
+#Also, had no idea what I'd need, so I just imported the commands I thought
+#would be useful. *shrugs*
+from .close_grabba import CloseGrabba
+from .drive_straight import DriveStraight
+from .lift_go_to_level import LiftGoToLevel
+from .mecanum_drive_with_joystick import MecanumDriveWithJoystick
+from .open_grabba import OpenGrabba
+from .set_grabba_setpoint import SetGrabbaSetpoint
+from .set_lift_setpoint import SetLiftSetpoint
+from .set_mast_setpoint import SetMastSetpoint
     
-    auto = Auto(self)
-        three_tote = self.smart_dashboard.getBoolean("3 Tote Auto", defaultValue=False) #If the dashboard hasn't set the value, it's False by default.
-        test_switch = self.smart_dashboard.getBoolean("Test Switch", defaultValue=False)
+class Autonomous(CommandGroup):
+    #Should really put some setpoints in. Ehhhhhhh...
+    def __init___(self, robot)
+        super().__init__()
+        self.addSequential(DriveStraight(robot))
+        self.addSequential(SetGrabbaSetpoint(robot, 1))
+        self.addSequential(CloseGrabba(robot)) #grabs first tote
+        self.addSequential(SetLiftSetpoint(robot, 1))
+        self.addSequential(LiftGoToLevel(robot))
+        self.addSequential(DriveStraight(robot))
+        self.addSequential(OpenGrabba(robot)) #drops first tote on second tote
+        self.addSequential(SetLiftSetpoint(robot, -1))
+        self.addSequential(LiftGoToLevel(robot))
+        self.addSequential(CloseGrabba(robot)) #grabs second tote
+        self.addSequential(SetLiftSetpoint(robot, 2))
+        self.addSequential(LiftGoToLevel(robot))
+        self.addSequential(DriveStraight(robot))
+        self.addSequential(OpenGrabba(robot)) #drops first and second totes on third tote
+        self.addSequential(SetLiftSetpoint(robot, -2))
+        self.addSequential(LiftGoToLevel(robot))
+        self.addSequential(CloseGrabba(robot)) #grabs third tote
+        self.addSequential(DriveStraight(robot)) #drives around for a bit
 
-        self.drive.setSafetyEnabled(False)
-
-        if three_tote:
-            #Do the thing if the button's pushed
-            auto.tote_grabba()
-            auto.tote_lift(1)
-            auto.can_slappa()
-            auto.forward(1)
-            auto.tote_releasa()
-            auto.tote_lower(1)
-            auto.tote_grabba()
-            auto.tote_lift(1)
-            auto.can_slappa()
-            auto.forward(1)
-            auto.tote_releasa()
-            auto.tote_lower(1)
-            auto.tote_grabba()
-            auto.tote_lift(1)
-
-        elif auto_program_two: #this is the simple auton that was talked about.
-            auto.forward(5)
-        else:
-            pass
-            #Neither are pushed
+    self.drive.setSafetyEnabled(False)
+    
--- commands/drive_straight.py
+++ commands/drive_straight.py
@@ -1,5 +1,6 @@
 __author__ = 'auxiliary-character'
 import wpilib.command.PIDCommand
+
 class DriveStraight(PIDCommand):
     def __init__(self, robot, x, y, angle, name=None, timeout=None):
         self.robot = robot
@@ -8,12 +9,31 @@ class DriveStraight(PIDCommand):
         self.angle = angle
         self.setSetpoint(angle)
         self.requires(self.robot.drivetrain)
-        super.__init__(name,timeout)
+        self.setTimeout(timeout)
+        super.__init__(1, 0, 0)
     
     def isFinished(self):
         if self.isTimedOut():
             self.robot.drivetrain.driveManual(0,0,0)
+            return True
+        else:
+            return False
     
-    def returnPIDInput()
-        angle = self.gyro.getYaw()
+    def returnPIDInput(self):
+        angle = self.robot.drivetrain.gyro.getYaw()
+        set_point = self.getSetpoint()
         
+        angle_greater = angle + 360
+        angle_lesser = angle - 360
+ 
+        use_angle = math.fabs(angle-set_point) < math.fabs(angle_greater - set_point)
+        use_angle = use_angle and math.fabs(angle-set_point) < math.fabs(angle_lesser - set_point)
+        if use_angle:
+            return angle
+        elif math.fabs(angle_greater-set_point) < math.fabs(angle_lesser - set_point):
+            return angle_greater
+        else:
+            return angle_lesser
+        
+    def usePIDInput(self, output):
+        self.robot.drivetrain.driveManual(x, y, output)
