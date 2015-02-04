10c875a117393a6968f3704c6ae9ff8c1c3b6880
nikolojedison <nikolojedison@gmail.com>
2015-02-04 00:27:05 +0000

    Stuff and things, feat. Aux

--- commands/drive_straight.py
+++ commands/drive_straight.py
@@ -1,2 +1,19 @@
-__author__ = 'nikolojedison'
-#To change this template use Tools | Templates.
+__author__ = 'auxiliary-character'
+import wpilib.command.PIDCommand
+class DriveStraight(PIDCommand):
+    def __init__(self, robot, x, y, angle, name=None, timeout=None):
+        self.robot = robot
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.setSetpoint(angle)
+        self.requires(self.robot.drivetrain)
+        super.__init__(name,timeout)
+    
+    def isFinished(self):
+        if self.isTimedOut():
+            self.robot.drivetrain.driveManual(0,0,0)
+    
+    def returnPIDInput()
+        angle = self.gyro.getYaw()
+        
