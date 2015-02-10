905d8e587927e3f750d3152a48f4700fa9d32989
Nik <nikolojedison@gmail.com>
2015-02-10 13:30:15 -0600

    *sigh* more naming...

--- commands/set_claw_setpoint.py
+++ commands/set_claw_setpoint.py
@@ -2,28 +2,28 @@ __author__ = 'nikolojedison'
 
 from wpilib.command import Command
 
-class SetGrabbaSetpoint(Command):
+class SetClawSetpoint(Command):
 
     def __init__(self, robot, setpoint):
         super().__init__()
         self.robot = robot
 
         self.setpoint = setpoint
-        self.requires(self.robot.grabber)
+        self.requires(self.robot.claw)
 
     def initialize(self):
-        self.robot.grabber.enable()
-        self.robot.grabber.setSetpoint(self.setpoint)
+        self.robot.claw.enable()
+        self.robot.claw.setSetpoint(self.setpoint)
 
     def execute(self):
         """Called repeatedly"""
 
     def isFinished(self):
-        return self.robot.grabber.onTarget() #Stay on target...
+        return self.robot.claw.onTarget() #Stay on target...
 
     def end(self):
         """Called once after isFinisherd returns true"""
-        self.robot.grabber.disable()
+        self.robot.claw.disable()
 
     def interrupted(self):
         """Called when another thing which requires one or more of the same subsyses is
