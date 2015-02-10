77d5c8804f34c49c292dca3b36bfac9ce7e2d933
Nik <nikolojedison@gmail.com>
2015-02-10 13:28:36 -0600

    More naming.

--- commands/manual_claw.py
+++ commands/manual_claw.py
@@ -1,18 +1,18 @@
 from wpilib.command import Command
 from drive_control import dead_zone
 
-class ManualGrabba(Command):
+class ManualClaw(Command):
     def __init__(self, robot):
         super().__init__()
         self.robot = robot
-        self.requires(self.robot.grabber)
+        self.requires(self.robot.claw)
 
     def execute(self):
-        self.robot.grabber.manualSet(dead_zone(self.robot.oi.getJoystickLeft().getThrottle(), .1))
+        self.robot.claw.manualSet(dead_zone(self.robot.oi.getJoystickLeft().getThrottle(), .1))
 
     def isFinished(self):
         return False
 
     def cancel(self):
-        self.robot.grabber.manualSet(0)
+        self.robot.claw.manualSet(0)
         super().cancel()
