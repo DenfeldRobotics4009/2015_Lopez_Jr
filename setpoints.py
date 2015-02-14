e5003b93794734d87c50133cc3e1e1abb252dc06
nikolojedison <nikolojedison@gmail.com>
2015-02-14 11:39:10 -0600

    Haaaaaaaaaaaands.

--- setpoints.py
+++ setpoints.py
@@ -7,7 +7,7 @@ kMastParallel = .416
 #claw setpoints - tested 2/12/15
 kOpen = .200
 kClose = .925 #2/12/15 23.12: .771 2/13/15 16:30: .925
-kCan = .5 #untested
+kCan = .375
 kTote = .505
 kStall = 2 #This is for the current sensor.
 
