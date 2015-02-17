519a28d71ca283a8029b5a85ab4aa14c835e3430
nikolojedison <nikolojedison@gmail.com>
2015-02-17 12:06:01 -0600

    Loader generator streamlining.

--- commands/tote_loader.py
+++ commands/tote_loader.py
@@ -8,19 +8,20 @@ from .open_claw import OpenClaw
 from .shaker import Shaker
 from wpilib.command import WaitCommand
 from .drive_straight import DriveStraight
+from set_lift_setpoint import SetLiftSetpoint
 
 class ToteLoader(CommandGroup):
     """A command that opens the arms, lowers the lift one tote level, grabs a tote, and raises the lift 1.1 levels."""
     def __init__(self, robot):
         super().__init__()
         loader_generator = [
-                LiftGoToLevel(robot, 7),
+                SetLiftSetpoint(robot, setpoints.kAboveFirst),
                 OpenClaw(robot),
                 LiftGoToLevel(robot, 2),
                 DriveStraight(robot, .5, 0, .5),
                 GrabTote(robot),
                 DriveStraight(robot, -.5, 0, .5),
                 GrabTote(robot),
-                LiftGoToLevel(robot, 8),
+                SetLiftSetpoint(robot, setpoints.kAboveSecond),
                 ]
         for i in loader_generator: self.addSequential(i)
