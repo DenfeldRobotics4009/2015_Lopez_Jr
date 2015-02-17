76aa89aa3b536840ba03d683444b7a9dd9276f34
nikolojedison <nikolojedison@gmail.com>
2015-02-16 19:55:19 -0600

    Setpoints updated.

--- setpoints.py
+++ setpoints.py
@@ -7,10 +7,10 @@ kMastForwardLimit = .482
 kMastParallel = .416
 
 #claw setpoints - tested 2/12/15
-kOpen = .285
-kClose = .976 #2/12/15 23.12: .771 2/13/15 16:30: .925 2/15/15 .070
-kCan = .375
-kTote = .625
+kOpen = .251
+kClose = .973 #2/12/15 23.12: .771 2/13/15 16:30: .925 2/15/15 .070
+kCan = .458
+kTote = .537
 kStall = 2 #This is for the current sensor.
 
 #lift setpoints - untested
