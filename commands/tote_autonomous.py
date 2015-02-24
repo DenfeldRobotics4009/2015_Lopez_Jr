dfa335795a9c7f82e7028909b842f0106d3316bf
nikolojedison <nikolojedison@gmail.com>
2015-02-24 22:00:41 +0000

    One tote auton added, needs testing.

---
+++ commands/tote_autonomous.py
@@ -0,0 +1,34 @@
+__author__ = 'nikolojedison'
+
+from wpilib.command import CommandGroup
+
+from .grab_tote import GrabTote
+from .drive_straight import DriveStraight
+from .lift_go_to_level import LiftGoToLevel
+from .mecanum_drive_with_joystick import MecanumDriveWithJoystick
+from .open_claw import OpenClaw
+from .set_claw_setpoint import SetClawSetpoint
+from .set_lift_setpoint import SetLiftSetpoint
+from .set_mast_setpoint import SetMastSetpoint
+from .turn import Turn
+from .lift_stuff import LiftStuff
+from .lift_go_to_level import LiftGoToLevel
+
+class ToteAutonomous(CommandGroup):
+    """This is the autonomous where we grab the can and drive off with it."""
+    #Clamp, lift up, turn 45 deg left, go forward, drop can
+    def __init__(self, robot):
+        super().__init__()
+        auton_generator = [
+            LiftGoToLevel(robot, 2),
+            GrabTote(robot), #grabs can
+            LiftGoToLevel(robot, 4),
+            DriveStraight(robot, 0, -.5, timeout=1),
+            Turn(robot, -45),
+            DriveStraight(robot, 0, -.5, timeout=2.5),
+            LiftGoToLevel(robot, 2),
+            OpenClaw(robot), #drops can
+            DriveStraight(robot, 0, .5, timeout=.25)]
+
+        #generates the autonomous (reeeeeally cool command, thanks Austin!)
+        for i in auton_generator: self.addSequential(i)
